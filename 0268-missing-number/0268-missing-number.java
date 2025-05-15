d3bba23211bef9f6831ff45aab86d4219cba20f7
Prudhvi Raj Baki <21761a1206@lbrce.ac.in>
2025-05-15 16:04:36 +0530

    Time: 0 ms (100%), Space: 45.2 MB (84.49%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int ans = 0;
+
+        for(int i : nums)   ans ^= i;
+
+        for(int i = 0; i <= nums.length; i++)   ans ^= i;
+
+        return ans;
+    }
+}
