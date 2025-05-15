037500c17f86113e7b88fbad2ab7b34c63014aa3
Prudhvi Raj Baki <21761a1206@lbrce.ac.in>
2025-05-15 15:34:33 +0530

    Time: 0 ms (100%), Space: 40.8 MB (15.29%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,9 +1,9 @@
 class Solution {
     public int getSum(int a, int b) {
         while(b != 0){
-            int carry  = (a & b) << 1;
+            int temp = (a & b) << 1;
             a = a ^ b;
-            b = carry;
+            b = temp;
         }
         return a;
     }
