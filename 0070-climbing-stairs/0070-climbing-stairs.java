f0f44f89c24c222403de27e03434e182b50b5f09
Prudhvi Raj Baki <21761a1206@lbrce.ac.in>
2025-05-20 16:08:57 +0530

    Time: 0 ms (100%), Space: 40.8 MB (10.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[3];
+        dp[ 0 ] = 1;
+        dp[ 1 ] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[ ( i ) % 3] = dp[( i - 1 ) % 3 ] + dp[ ( ( i - 2 ) % 3 ) ];
+        }
+        return dp[ n % 3 ];
+    }
+}
