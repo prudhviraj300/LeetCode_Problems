abdf1b0959587ec8945d4fe3404040ddcfede850
Prudhvi Raj Baki <21761a1206@lbrce.ac.in>
2025-05-20 15:47:06 +0530

    Time: 0 ms (100%), Space: 41.7 MB (86.61%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,9 +1,8 @@
 public class Solution {
-
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int ans = 0;
-        for (int i = 0; i < 32; i++) {
+        for(int i = 0; i < 32; i++){
             int temp = (n & 1);
             ans = ans << 1;
             ans |= temp;
