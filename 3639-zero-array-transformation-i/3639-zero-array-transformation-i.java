274c4f1d97bfb329de983a62157df019492deddd
Prudhvi Raj Baki <21761a1206@lbrce.ac.in>
2025-05-20 15:41:33 +0530

    Time: 14 ms (9.76%), Space: 99.8 MB (5.4%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        // [ 0 1 2 3 ]
+        // [ -1 -1 0 +1 ]  [ -1 -2 -2 -1]
+        // [ 4 3 2 1 ]
+        // [ 3 1 0 0 ]
+        int n = nums.length;
+        List<Integer> pre = new ArrayList<>(Collections.nCopies(n, 0));
+        for (int[] ele : queries) {
+            int l = ele[0];
+            int r = ele[1];
+            pre.set(l, pre.get(l) - 1);
+            if ((r + 1 < n)) {
+                pre.set(r + 1, pre.get(r + 1) + 1);
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            pre.set(i, pre.get(i - 1) + pre.get(i));
+        }
+        boolean flag = true;
+        for (int i = 0; i < n; i++) {
+            if((nums[i] + pre.get(i)) > 0){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
