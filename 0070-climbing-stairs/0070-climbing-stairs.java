a8c5cb86ee0f610bafbf2af5a32ab96b85a38788
Prudhvi Raj Baki <21761a1206@lbrce.ac.in>
2025-05-21 21:48:20 +0530

    Time: 0 ms (100%), Space: 40.3 MB (66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,28 @@
 class Solution {
     public int climbStairs(int n) {
-        // 
-        int[] dp = new int [n + 1];  // [0 1 2 3 4 5 ]
-                                     // [1 1 2 3 5 8 ]
+        // tabulation
+        // int[] dp = new int [n + 1];  // [0 1 2 3 4 5 ]
+        //                              // [1 1 2 3 5 8 ]
+        // dp[0] = 1;
+        // dp[1] = 1;
+        //                             // [ 1 1 2 3 5 8]
+        // for(int i = 2; i <= n; i++){
+        //     dp[i] = dp[i - 1]  + dp[i - 2];
+        // }
+        // // tc = o(n) sc = o(1)
+
+        // return dp[n];
+
+
+        int [] dp = new int[3]; // [ 0 1 2 ]
+
         dp[0] = 1;
         dp[1] = 1;
-                                    // [ 1 1 2 3 5 8]
+
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1]  + dp[i - 2];
+            dp[i % 3] = dp[(i - 1) % 3] + dp[(i - 2) % 3];
         }
 
-
-        return dp[n];
+        return dp[n % 3];
     }
 }
