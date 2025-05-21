4f9154e920827a8836e883d9b87dfbc5cc4c2ba7
Prudhvi Raj Baki <21761a1206@lbrce.ac.in>
2025-05-21 21:22:39 +0530

    Time: 3 ms (7.18%), Space: 45.8 MB (46.88%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -7,8 +7,8 @@ class Solution {
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    rows.add(i);
-                    cols.add(j);
+                    if (!rows.contains(i))  rows.add(i);
+                    if (!cols.contains(j))  cols.add(j);
                 }
             }
         }
