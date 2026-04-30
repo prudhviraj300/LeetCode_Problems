8ef60829df57a941df7d3bb1c498ded0e222fbf8
Prudhvi Raj Baki <21761a1206@lbrce.ac.in>
2026-04-30 18:16:29 +0530

    Time: 6 ms (41.71%), Space: 14.8 MB (54.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,17 @@
-#include <unordered_map>
- 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;
-       
+
         for(int i = 0; i < nums.size(); i++){
-            if(mp.find(target - nums[i]) == mp.end())
+            if(mp.find(target - nums[i]) == mp.end()){
                 mp[nums[i]] = i;
-            else
-                return {mp[target - nums[i]], i};
+            }else{
+                return {i, mp[target - nums[i]]};
+            }
         }
- 
+
         return {-1, -1};
+        
     }
 };
